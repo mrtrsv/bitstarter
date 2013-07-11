9654b88afaa963f8076383fd45e19b8c13a1620e
Mart Rasva <mart.rasva@gmail.com>
2013-07-11 13:26:27 +0000

    read from index.html-fixed

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFile('index.html', function (err, data) {
- 	response.send(data);
+ 	response.send(data.toString('utf-8'));
   });
 });
 
