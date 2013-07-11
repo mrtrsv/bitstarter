8b9bcdac0b13aac0e09a24b90df9aa1dc8c70785
Mart Rasva <mart.rasva@gmail.com>
2013-07-11 13:17:11 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function (err, data) {
+ 	response.send(data);
+  });
 });
 
 var port = process.env.PORT || 5000;
